8762c35a98d5ee78df1b501ff603719b107c6497
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2017-10-11 00:04:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main ()
 {
 	std::cout << "Let`s play 2048!\n" << std::endl;
 	
-	int matrix[4][4], i, j, count=1, m=0;
+	int matrix[4][4], i, j, count=1, l=1, m=0;
 	char op;
 
 	for ( i = 0; i < 4; i++) {
@@ -45,9 +45,24 @@ sluchay:i=rand()%4;
 	
 	std::cin >> op;
 	
-	while(count > 0) {
+	while(l > 0) {
 		
 		std::cout << std::endl;
+		
+		l = 0;
+		
+		for (i=0; i<4; i++){
+			for (j=0; j<4; j++)
+				if (matrix[i][j] == matrix[i+1][j] || matrix[i][j] == matrix[i][j+1] || matrix[i][j] == 0) {
+					l = 1;
+					break;
+				}
+				if (l == 1) break;
+		}
+		
+		if (l != 1) break;
+		
+		count = 0;
 		
 begin: switch (op)
 		{
@@ -60,6 +75,7 @@ begin: switch (op)
 							if (matrix[k][j] != 0){
 								matrix[i][j] = matrix[k][j];
 								matrix[k][j]=0;
+								count=1;
 								break;
 							}
 						}
@@ -70,10 +86,11 @@ begin: switch (op)
 				for (j=0; j<4; j++) {
 					
 					for (i=3; i>0; i--){
-						if (matrix[i][j] == matrix[i-1] [j]) {
+						if (matrix[i][j] == matrix[i-1] [j] && matrix[i][j] != 0) {
 							matrix[i][j] += matrix[i-1][j];
 							matrix[i-1][j]=0;
 							m+=matrix[i][j];
+							count = 1;
 						}
 					}
 				}
@@ -104,6 +121,7 @@ begin: switch (op)
 							if (matrix[k][j] != 0){
 								matrix[i][j] = matrix[k][j];
 								matrix[k][j]=0;
+								count=1;
 								break;
 							}
 						}
@@ -112,10 +130,11 @@ begin: switch (op)
 				
 				for (j=0; j<4; j++) {
 					for (i=0; i<3; i++)
-					if (matrix[i][j] == matrix[i+1] [j]) {
+					if (matrix[i][j] == matrix[i+1] [j] && matrix[i][j] != 0) {
 						matrix[i][j] += matrix[i+1][j];
 						matrix[i+1][j]=0;
 						m+=matrix[i][j];
+						count=1;
 					}
 				}
 				
@@ -144,6 +163,7 @@ begin: switch (op)
 							if (matrix[i][k] != 0){
 								matrix[i][j] = matrix[i][k];
 								matrix[i][k]=0;
+								count=1;
 								break;
 							}
 						}
@@ -152,10 +172,11 @@ begin: switch (op)
 				
 				for (i=0; i<4; i++) {
 					for (j=0; j<3; j++)
-					if (matrix[i][j] == matrix[i] [j+1]) {
+					if (matrix[i][j] == matrix[i] [j+1] && matrix[i][j] != 0) {
 						matrix[i][j] += matrix[i][j+1];
 						matrix[i][j+1]=0;
 						m+=matrix[i][j];
+						count=1;
 					}
 				}
 			
@@ -184,6 +205,7 @@ begin: switch (op)
 							if (matrix[i][k] != 0){
 								matrix[i][j] = matrix[i][k];
 								matrix[i][k]=0;
+								count=1;
 								break;
 							}
 						}
@@ -192,10 +214,11 @@ begin: switch (op)
 				
 				for (i=0; i<4; i++) {
 					for (j=3; j>0; j--)
-					if (matrix[i][j] == matrix[i] [j-1]) {
+					if (matrix[i][j] == matrix[i] [j-1] && matrix[i][j] != 0) {
 						matrix[i][j] += matrix[i][j-1];
 						matrix[i][j-1]=0;
 						m+=matrix[i][j];
+						count=1;
 					}
 				}
 				
@@ -217,6 +240,7 @@ begin: switch (op)
 			
 			default: {
 				std::cout << "You`ve printed the wrong symbol! Please, print it again: ";
+				count = 1;
 				std::cin >> op;
 				std::cout << std::endl;
 				goto begin;
@@ -224,32 +248,45 @@ begin: switch (op)
 			
 		}
 		
-again:	i=rand()%4;
-		j=rand()%4;
+		if (count == 1){
 
-		if (matrix[i][j] == 0) {
-		if (rand()%10 <=8) 	matrix[i][j]=2;
-		else matrix [i][j]=4;
-		}
-		else goto again;
+			again:	i=rand()%4;
+					j=rand()%4;
+
+					if (matrix[i][j] == 0) {
+						if (rand()%10 <=8) 	matrix[i][j]=2;
+						else matrix [i][j]=4;
+						}
+					else goto again;
 	
-		for ( i = 0; i < 4; i++) {
-		for ( j = 0; j < 4; j++) {
-		if (matrix[i][j] == 0){
-			std::cout << "*  ";
-		}
+					for ( i = 0; i < 4; i++) {
+						for ( j = 0; j < 4; j++) {
+							if (matrix[i][j] == 0){
+								std::cout << "*  ";
+							}
+							else {
+								std::cout << matrix[i][j] << "  ";
+							}
+						}
+				
+						std::cout << std::endl;
+					}
+				
+					std::cout << "Your goals are: " << m << std::endl << "Press your letter: ";
+					}
 		else {
-			std::cout << matrix[i][j] << "  ";
+
+			count = 1;
+			std::cout << "You can`t make this action. Please, choose another letter: ";
 		}
-		}
-				
-		std::cout << std::endl;
-		}
-				
-		std::cout << "Your goals are: " << m << std::endl << "Press your letter: ";
+
 				
 		std::cin >> op;
 	}
+	
+	std::cout << "Sorry, you lose! Thanks for the good game! Your goals are: " << m;
+	
+	std::cin.get();
 	
 	return 0;
 }
